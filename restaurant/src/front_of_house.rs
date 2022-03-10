408d4a6adfe3e98530edf91dbb058d1e5a352d99
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-03-10 10:24:07 +0900

    7.5

---
+++ restaurant/src/front_of_house.rs
@@ -0,0 +1,6 @@
+// 別ファイルから読み込み
+pub mod hosting;
+pub mod serving;
+
+// 空
+mod front_of_house {}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,36 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+mod front_of_house;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {
-            println!("add_to_waitlist")
-        }
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Absolute path
     // 絶対パス
-    crate::front_of_house::hosting::add_to_waitlist();
+    //crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // Relative path
     // 相対パス
-    front_of_house::hosting::add_to_waitlist();
+    //front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
