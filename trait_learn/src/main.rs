95d54b3727ef06f0a10f44783adbaff7c2f37852
Tim0401 <36380433+Tim0401@users.noreply.github.com>
2022-03-22 02:29:27 +0900

    10.2 trait

---
+++ trait_learn/src/main.rs
@@ -0,0 +1,14 @@
+use trait_learn::{Summary, Tweet};
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            // もちろん、ご存知かもしれませんがね、みなさん
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
