2aac92658334aefc8297718d4f3f7287f5427927
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-04-12 10:01:38 +0900

    12.6

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,19 +5,14 @@ use minigrep::{run, Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
-        // 引数解析時に問題
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    // --snip--
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
