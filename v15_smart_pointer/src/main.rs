86329a60f298907179a508808b244952bf33c446
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-04-21 10:17:34 +0900

    15.2

--- v15_smart_pointer/src/main.rs
+++ v15_smart_pointer/src/main.rs
@@ -6,10 +6,56 @@ fn main() {
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list = {:?}", list);
+
+    // 15.2 deref
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
 
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+// 要素1のタプル
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        // タプルの0番目を返す
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
 }
