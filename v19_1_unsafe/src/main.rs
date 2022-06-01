a7480fcf9033855f464834b6aef9a16bdd8bad78
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-06-01 10:15:31 +0900

    unsafe2

--- v19_1_unsafe/src/main.rs
+++ v19_1_unsafe/src/main.rs
@@ -1,3 +1,32 @@
+use std::slice;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    //  任意のメモリアドレスからスライスを生成する(危険)
+    let address = 0x012345usize;
+    let r = address as *mut i32;
+    let slice = unsafe { slice::from_raw_parts_mut(r, 10000) };
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = slice.len();
+    let ptr = slice.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),
+        )
+    }
 }
