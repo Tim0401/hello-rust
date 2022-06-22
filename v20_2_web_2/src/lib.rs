2bd1e36a63d50d2fb51618c81b85ca14e0f2ce52
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2022-06-22 10:43:42 +0900

    20.2

--- v20_2_web_2/src/lib.rs
+++ v20_2_web_2/src/lib.rs
@@ -1,8 +1,14 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// 新しいThreadPoolを生成する。
@@ -14,19 +20,24 @@ impl ThreadPool {
     /// sizeが0なら、`new`関数はパニックします。
     pub fn new(size: usize) -> ThreadPool {
         let mut workers = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
 
         for id in 0..size {
             // スレッドを生成してベクタに格納する
             // create some threads and store them in the vector
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -36,8 +47,17 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                // ワーカー{}は仕事を得ました; 実行します
+                println!("Worker {} got a job; executing.", id);
+
+                job();
+            }
+        });
 
         Worker { id, thread }
     }
